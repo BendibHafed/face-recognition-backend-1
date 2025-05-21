84cc6cd5f121c1221e0c3d2612affdbc629cec61
BendibHafed <hafededdinebendib@gmail.com>
2025-05-21 14:11:32 +0200

    fix node-fetch dependency

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    'https://bendibhafed.github.io',
+    'https://bendibhafed.github.io/face-recognition-frontend-1/',
     'http://localhost:5173'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
